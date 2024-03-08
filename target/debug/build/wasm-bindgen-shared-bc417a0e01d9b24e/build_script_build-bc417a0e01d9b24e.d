74b94c5c97a4ae0b54fb26a38501ab5437823848
vition vlad <vladvition@gmail.com>
2024-03-08 21:36:48 +0200

    delete

--- target/debug/build/wasm-bindgen-shared-bc417a0e01d9b24e/build_script_build-bc417a0e01d9b24e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\vladv\OneDrive\Documents\GitHub\schnellwerke\target\debug\build\wasm-bindgen-shared-bc417a0e01d9b24e\build_script_build-bc417a0e01d9b24e.exe: C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\wasm-bindgen-shared-0.2.91\build.rs
-
-C:\Users\vladv\OneDrive\Documents\GitHub\schnellwerke\target\debug\build\wasm-bindgen-shared-bc417a0e01d9b24e\build_script_build-bc417a0e01d9b24e.d: C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\wasm-bindgen-shared-0.2.91\build.rs
-
-C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\wasm-bindgen-shared-0.2.91\build.rs:
--- target/debug/deps/unicode_ident-9f322f922d10e7d9.d
+++
@@ -1,8 +0,0 @@
-C:\Users\vladv\OneDrive\Documents\GitHub\schnellwerke\target\debug\deps\libunicode_ident-9f322f922d10e7d9.rmeta: C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-C:\Users\vladv\OneDrive\Documents\GitHub\schnellwerke\target\debug\deps\libunicode_ident-9f322f922d10e7d9.rlib: C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-C:\Users\vladv\OneDrive\Documents\GitHub\schnellwerke\target\debug\deps\unicode_ident-9f322f922d10e7d9.d: C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs
-
-C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\lib.rs:
-C:\Users\vladv\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.12\src\tables.rs:
